7bea200009e0f479812f9e425b06f462eb00da17
bishop777-sys <bishop_777@rambler.ru>
2020-05-08 04:29:09 +0300

    Add pattern Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.GetInstance().GetHashCode());
+            Console.WriteLine(Singleton.GetInstance().GetHashCode());
+            Console.ReadKey();
+        }
+    }
+
+
+    class Singleton
+    {
+        private static Singleton singleClass;
+
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (singleClass == null)
+                singleClass = new Singleton { };
+            return singleClass;
+        }
+    }
+}
